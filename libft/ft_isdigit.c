6eff257c14c07a4c4ef70375c68ace923f5bfb0d
son <guswns8431@gmail.com>
2020-12-22 18:19:51 +0900

    ft_isdigit.c

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/22 12:21:48 by hyson             #+#    #+#             */
-/*   Updated: 2020/12/22 17:41:37 by hyson            ###   ########.fr       */
+/*   Created: 2020/12/22 17:40:18 by hyson             #+#    #+#             */
+/*   Updated: 2020/12/22 17:41:17 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-
-void	*ft_memset(void *dest, int value, size_t size);
-void	ft_bzero(void *dest, size_t size);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
