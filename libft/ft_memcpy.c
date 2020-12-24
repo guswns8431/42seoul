504c721fb6ab3d3d74d5de7c81160a491348d43c
son <guswns8431@gmail.com>
2020-12-24 22:42:05 +0900

    ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/22 12:21:48 by hyson             #+#    #+#             */
-/*   Updated: 2020/12/24 20:29:52 by hyson            ###   ########.fr       */
+/*   Created: 2020/12/24 20:16:08 by hyson             #+#    #+#             */
+/*   Updated: 2020/12/24 22:34:02 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*pdst;
+	const char	*psrc;
 
-void	*ft_memset(void *dest, int value, size_t size);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	ft_bzero(void *dest, size_t size);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-#endif
+	pdst = dst;
+	psrc = src;
+	if (!dst && !src)
+		return (0);
+	while (n-- > 0)
+		*pdst++ = *psrc++;
+	return (dst);
+}
