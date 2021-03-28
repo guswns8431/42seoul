00dc76651120a6be8d9f2587f43ef0165c7b490e
guswns8431 <guswns8431@gmail.com>
2021-03-28 19:56:07 +0900

    %i

--- printf/ft_check_type.c
+++ printf/ft_check_type.c
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 14:37:18 by hyson             #+#    #+#             */
-/*   Updated: 2021/03/28 17:14:28 by hyson            ###   ########.fr       */
+/*   Updated: 2021/03/28 19:55:56 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ void	ft_check_type(char *c, t_option *val, va_list ap)
 		ft_print_type_c(val, ap);
 	else if (*c == 's')
 		ft_print_type_s(val, ap);
-	else if (*c == 'd')
+	else if (*c == 'd' || *c == 'i')
 		ft_print_type_d(val, ap);
 	/*
 	else if (*c == 'p')
-	else if (*c == 'i')
 	else if (*c == 'u')
 	else if (*c == 'x')
 	else if (*c == 'X')
