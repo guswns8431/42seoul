29daa7afe1d6280aebf7c8207e2b766732d50376
son <guswns8431@gmail.com>
2021-04-26 17:23:10 +0900

    %u도 %i%d와 같은 코드로 해결했고 norminette해결을 하지 못 했음

--- printf/ft_print_type_u.c
+++ printf/ft_print_type_u.c
@@ -6,30 +6,53 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 19:33:55 by hyson             #+#    #+#             */
-/*   Updated: 2021/04/16 22:28:56 by hyson            ###   ########.fr       */
+/*   Updated: 2021/04/26 17:21:24 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+static	int	ft_max(int pre, int len)
+{
+	if (pre > len)
+		len = pre;
+	return (len);
+}
+
 void	ft_print_type_u(t_option *val, va_list ap)
 {
 	unsigned int		nbr;
 	int		len;
+	int		tmp;
 
+	len = 0;
 	nbr = va_arg(ap, unsigned int);
+	tmp = val->width;
+	if (nbr == 0 && val->precision == 0 && val->dot == 1)
+	{
+		while (tmp--)
+			val->len += write(1, " ", 1);
+		return ;
+	}
 	len = ft_nbrlen(nbr, 10);
-	val->len += len;
 	if (val->minus == 1)
 	{
+		if (val->negative)
+			val->len += write(1, "-", 1);
 		ft_process_precision_num(val->precision, len);
+		len = ft_max(val->precision, len);
+		val->len += len;
 		ft_putnbr_base(nbr, "0123456789");
-		ft_process_width(len, val->width, val->zero, val->dot);
+		val->len += ft_process_width_num(len, 0, val);
 	}
 	else
 	{
-		ft_process_width(len, val->width, val->zero, val->dot);
-		ft_process_precision_num(val->precision, len);
+		len = ft_max(val->precision, len);
+		val->len += len;
+		val->len += ft_process_width_num(len, val->zero, val);
+		if (val->negative)
+			val->len += write(1, "-", 1);
+		ft_process_precision_num(val->precision, ft_nbrlen(nbr, 10));
 		ft_putnbr_base(nbr, "0123456789");
 	}
 }
--- printf/main.c
+++ printf/main.c
@@ -8,9 +8,9 @@ int main(void)
 	int d = -1;
 	int make, real;
 
-	make = ft_printf("-->|%04d|<--\n", d);
+	make = ft_printf("-->|%-15.p|<--\n", a);
 	printf("\n");
-	real = printf("-->|%04d|<--\n", d);
+	real = printf("-->|%-15.p|<--\n", a);
 	printf("\n");
 	printf("make : %d, real : %d\n",make, real);
 	return (0);
