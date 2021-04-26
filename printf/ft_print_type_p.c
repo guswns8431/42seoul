a4cdf8b49b9137b45525b3b4a1862dd2857bad9f
son <guswns8431@gmail.com>
2021-04-26 20:10:16 +0900

    %p에 문제가 있음

--- printf/ft_print_type_p.c
+++ printf/ft_print_type_p.c
@@ -6,29 +6,49 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 19:33:55 by hyson             #+#    #+#             */
-/*   Updated: 2021/04/16 22:28:44 by hyson            ###   ########.fr       */
+/*   Updated: 2021/04/26 20:00:18 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+static	int	ft_max(int pre, int len)
+{
+	if (pre > len)
+		len = pre;
+	return (len);
+}
+
 void	ft_print_type_p(t_option *val, va_list ap)
 {
 	long long	nbr;
 	int			len;
+	int		tmp;
 
+	len = 0;
 	nbr = va_arg(ap, long long);
+	tmp = val->width;
+	if (nbr == 0 && val->precision == 0 && val->dot == 1)
+	{
+		while (tmp--)
+			val->len += write(1, " ", 1);
+		return ;
+	}
 	len = ft_nbrlen(nbr, 16);
 	val->len += len + 2;
 	if (val->minus == 1)
 	{
+		len = ft_max(val->precision, len);
+		val->len += len;
 		write(1, "0x", 2);
 		ft_putnbr_base(nbr, "0123456789abcdef");
-		ft_process_width(len + 2, val->width, val->zero, val->dot);
+		val->len += ft_process_width_p(len + 2, 0, val);
 	}
 	else
 	{
-		ft_process_width(len + 2, val->width, val->zero, val->dot);
+		len = ft_max(val->precision, len);
+		val->len += len;
+		ft_process_width_p(len + 2, val->zero, val);
 		write(1, "0x", 2);
 		ft_putnbr_base(nbr, "0123456789abcdef");
 	}
---
+++ printf/ft_process_width_p.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_process_width_p.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/24 17:03:43 by hyson             #+#    #+#             */
+/*   Updated: 2021/04/26 20:03:06 by hyson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int	ft_process_width_p(int len, int zero, t_option *val)
+{
+	int i;
+	int rt;
+
+	if (val->width != 0 && len < val->width)
+	{
+		i = val->width - len;
+		rt = i;
+		if (zero && !val->dot)
+		{
+			if (val->negative)
+			{
+				val->len += write(1, "-", 1);
+				val->negative = 0;
+			}
+			while (i--)
+				write(1, "0", 1);
+		}
+		else
+			while (i--)
+				write(1, " ", 1);
+		return (rt);
+	}
+	else
+		return (0);
+}
