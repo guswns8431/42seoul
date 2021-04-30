a1040db0b6fcb40b850f4778f0d5c45e0133de05
guswns8431 <guswns8431@gmail.com>
2021-04-30 17:00:31 +0900

    ft_print_type_d.c에 ft_process_minus추가해서 norminette해결

--- printf/ft_print_type_d.c
+++ printf/ft_print_type_d.c
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 19:33:55 by hyson             #+#    #+#             */
-/*   Updated: 2021/04/30 15:07:06 by hyson            ###   ########.fr       */
+/*   Updated: 2021/04/30 16:58:50 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,30 @@ static int	ft_max(int pre, int len)
 	if (pre > len)
 		len = pre;
 	return (len);
+}
+
+static void	ft_process_minus(long long nbr, t_option *val, int len)
+{
+	if (val->minus == 1)
+	{
+		if (val->negative)
+			val->len += write(1, "-", 1);
+		ft_process_precision_num(val->precision, len);
+		len = ft_max(val->precision, len);
+		val->len += len;
+		ft_putnbr_base(nbr, "0123456789");
+		val->len += ft_process_width_num(len, 0, val);
+	}
+	else
+	{
+		len = ft_max(val->precision, len);
+		val->len += len;
+		val->len += ft_process_width_num(len, val->zero, val);
+		if (val->negative)
+			val->len += write(1, "-", 1);
+		ft_process_precision_num(val->precision, ft_nbrlen(nbr, 10));
+		ft_putnbr_base(nbr, "0123456789");
+	}
 }
 
 void		ft_print_type_d(t_option *val, va_list ap)
@@ -41,24 +65,5 @@ void		ft_print_type_d(t_option *val, va_list ap)
 		return ;
 	}
 	len = ft_nbrlen(nbr, 10);
-	if (val->minus == 1)
-	{
-		if (val->negative)
-			val->len += write(1, "-", 1);
-		ft_process_precision_num(val->precision, len);
-		len = ft_max(val->precision, len);
-		val->len += len;
-		ft_putnbr_base(nbr, "0123456789");
-		val->len += ft_process_width_num(len, 0, val);
-	}
-	else
-	{
-		len = ft_max(val->precision, len);
-		val->len += len;
-		val->len += ft_process_width_num(len, val->zero, val);
-		if (val->negative)
-			val->len += write(1, "-", 1);
-		ft_process_precision_num(val->precision, ft_nbrlen(nbr, 10));
-		ft_putnbr_base(nbr, "0123456789");
-	}
+	ft_process_minus(nbr, val, len);
 }
