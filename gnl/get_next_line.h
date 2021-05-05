72742472031ccc6704b8590feac7c9d8785b4c99
guswns8431 <guswns8431@gmail.com>
2021-05-05 22:43:11 +0900

    gnl

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 19:45:48 by hyson             #+#    #+#             */
-/*   Updated: 2021/05/04 19:49:17 by hyson            ###   ########.fr       */
+/*   Updated: 2021/05/05 19:02:56 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <stdlib>
+# include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h> // 이건 빼야해
 
