f9dd89f6b86442c02ba28dc10a255ba64ba97a3e
guswns8431 <guswns8431@gmail.com>
2021-05-09 22:32:11 +0900

    오류처리

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 19:42:33 by hyson             #+#    #+#             */
-/*   Updated: 2021/05/04 20:05:02 by hyson            ###   ########.fr       */
+/*   Updated: 2021/05/09 22:31:53 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 
 int	get_next_line(int fd, char **line)
 {
+	static char *tmp;
+	
+	if (fd < 0 || !line || BUFFER_SIZE < 1)
+		return (-1);
 
+	printf("tmp : %s\n", tmp);
+	tmp = *line;
+	if(read(fd, *line, 10) == -1)
+		printf("read error\n");
+
+	printf("%s\n", *line);
+	return (0);
+}
+
+int main()
+{
+	int fd;
+	char *s;
+
+	fd = open("./test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("cannot open file\n");
+		return (1);
+	}
+	get_next_line(fd, &s);
+	get_next_line(fd, &s);
+	return (0);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 19:45:48 by hyson             #+#    #+#             */
-/*   Updated: 2021/05/05 19:02:56 by hyson            ###   ########.fr       */
+/*   Updated: 2021/05/07 17:53:37 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h> // 이건 빼야해
+# include <fcntl.h> // 이것도 빼야해
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 4096
+# endif
 
 #endif
