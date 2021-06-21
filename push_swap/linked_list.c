4c848f5e6d3f5730a470fbddec42a7ad04704622
son <guswns8431@gmail.com>
2021-06-21 17:04:47 +0900

    linked_list

--- push_swap/linked_list.c
+++ push_swap/linked_list.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct list{
-	int data;
-	struct list *next;
-} l_list;
-
-void insertnode(l_list *node, int val)
+typedef struct linked_list
 {
-	l_list *newnode = (l_list*)malloc(sizeof(l_list));
+	int value;
+	struct linked_list *next;
+} List;
 
-	node->next = newnode;
-	newnode->data = val;
-	newnode->next = NULL;
-}
-
-int main(void)
+int main()
 {
-	l_list *head = malloc(sizeof(l_list));
+	List *node1 = malloc(sizeof(struct linked_list));
+	List *node2 = malloc(sizeof(struct linked_list));
+	List *node3 = malloc(sizeof(struct linked_list));
 
-	l_list *node1 = malloc(sizeof(l_list));
-	head->next = node1;
-	node1->data = 1;
+	node1->value = 1;
+	node1->next = node2;
 
-	insertnode(node1, 2);
-	insertnode(node1, 3);
-	printf("%d %d %d", head->next->data, head->next->next->data, head->next->next->next->data);
+	node2->value = 2;
+	node2->next = node3;
+
+	node3->value = 3;
+	node3->next = NULL;
+
+	printf("%d %d %d\n", node1->value, node2->value, node3->value);
+	printf("%d %d %d\n", node1->value, node1->next->value, node1->next->next->value);
 	return (0);
 }
+
