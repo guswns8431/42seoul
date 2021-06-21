2d9069dba78ada493761ad25a25e29386b3df086
son <guswns8431@gmail.com>
2021-06-21 18:57:52 +0900

    linked_list

--- push_swap/linked_list.c
+++ push_swap/linked_list.c
@@ -9,21 +9,21 @@ typedef struct linked_list
 
 int main()
 {
-	List *node1 = malloc(sizeof(struct linked_list));
-	List *node2 = malloc(sizeof(struct linked_list));
-	List *node3 = malloc(sizeof(struct linked_list));
+	List node1; 
+	List node2;
+	List node3;
 
-	node1->value = 1;
-	node1->next = node2;
+	node1.value = 1;
+	node1.next = node2;
 
-	node2->value = 2;
-	node2->next = node3;
+	node2.value = 2;
+	node2.next = &node3;
 
-	node3->value = 3;
-	node3->next = NULL;
+	node3.value = 3;
+	node3.next = NULL;
 
-	printf("%d %d %d\n", node1->value, node2->value, node3->value);
-	printf("%d %d %d\n", node1->value, node1->next->value, node1->next->next->value);
+	printf("%d", node1.next->value);
+	printf("%d", node1.value);
+
 	return (0);
 }
-
