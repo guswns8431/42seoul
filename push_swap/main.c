b4faede49b099ecc934f540d38b078c47d3d0346
son <guswns8431@gmail.com>
2021-06-25 21:02:40 +0900

    중복체크 버그

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,11 +6,22 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 18:12:28 by hyson             #+#    #+#             */
-/*   Updated: 2021/06/25 19:35:26 by hyson            ###   ########.fr       */
+/*   Updated: 2021/06/25 21:00:40 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int		ft_isnum(char *s)
+{
+	int i;
+
+	i = -1;
+	while(s[++i])
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	return (1);
+}
 
 int		ft_strcmp(char *s1, char *s2)
 {
@@ -21,7 +32,7 @@ int		ft_strcmp(char *s1, char *s2)
 	return (1);
 }
 
-void	check_error(int argc, char **argv)
+void	ft_check_error(int argc, char **argv)
 {
 	int i;
 	int j;
@@ -29,19 +40,21 @@ void	check_error(int argc, char **argv)
 	i = 0;
 	while (++i < argc - 1)
 	{
-		j = i + 1;
+		j = i;
 		while (++j < argc)
-			if (ft_strcmp(argv[i], argv[j]))
+		{
+			if (!ft_isnum(argv[j]) || ft_strcmp(argv[i], argv[j]))
 			{
 					write(2, "Error\n", 6);
 					exit(0);
 			}
+		}
 	}
 }
 
 
 int		main(int argc, char **argv)
 {
-	check_error(argc, argv);
+	ft_check_error(argc, argv);
 	return (0);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 16:11:34 by hyson             #+#    #+#             */
-/*   Updated: 2021/06/25 19:40:44 by hyson            ###   ########.fr       */
+/*   Updated: 2021/06/25 20:16:32 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 # include <stdio.h> //빼야해
 
-int	ft_strcmp(char *s1, char *s2);
-void	check_error(int argc, char **argv);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_check_error(int argc, char **argv);
 
 #endif
