22603c83a94f918aadd23f73ffb304d03462694e
son <guswns8431@gmail.com>
2021-06-28 22:30:27 +0900

    인자값이 음수일때 이상함

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,11 +6,25 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 18:12:28 by hyson             #+#    #+#             */
-/*   Updated: 2021/06/28 18:40:29 by hyson            ###   ########.fr       */
+/*   Updated: 2021/06/28 22:27:57 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int		ft_isinteger(char *s)
+{
+	int i;
+	long long num;
+
+	i = -1;
+	num = 0;
+	while (s[++i])
+		num = num * 10 + (s[i] - '0');
+	if (num >= -2147483648 && num <= 2147483647)
+		return (1);
+	return (0);
+}
 
 int		ft_isnum(char *s)
 {
@@ -38,12 +52,17 @@ void	ft_check_error(int argc, char **argv)
 	int j;
 
 	i = 0;
-	while (++i < argc - 1)
+	while (++i < argc)
 	{
+		if (!ft_isnum(argv[i]) || !ft_isinteger(argv[i]))
+		{
+			write(2, "Error\n", 6);
+			exit(0);
+		}
 		j = i;
 		while (++j < argc)
 		{
-			if (!ft_isnum(argv[j]) || ft_strcmp(argv[i], argv[j]))
+			if (ft_strcmp(argv[i], argv[j]))
 			{
 					write(2, "Error\n", 6);
 					exit(0);
