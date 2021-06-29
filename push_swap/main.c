ab7e750e76761b34f0091b9e0883cec97ee9fc99
guswns8431 <guswns8431@gmail.com>
2021-06-29 16:16:31 +0900

    norm

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 18:12:28 by hyson             #+#    #+#             */
-/*   Updated: 2021/06/28 22:27:57 by hyson            ###   ########.fr       */
+/*   Updated: 2021/06/29 16:16:14 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		ft_isinteger(char *s)
 {
-	int i;
-	long long num;
+	int			i;
+	long long	num;
 
 	i = -1;
 	num = 0;
@@ -28,10 +28,10 @@ int		ft_isinteger(char *s)
 
 int		ft_isnum(char *s)
 {
-	int i;
+	int	i;
 
 	i = -1;
-	while(s[++i])
+	while (s[++i])
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
 	return (1);
@@ -39,7 +39,9 @@ int		ft_isnum(char *s)
 
 int		ft_strcmp(char *s1, char *s2)
 {
-	int i = -1;
+	int	i;
+
+	i = -1;
 	while (s1[++i] || s2[i])
 		if (s1[i] != s2[i])
 			return (0);
@@ -48,8 +50,8 @@ int		ft_strcmp(char *s1, char *s2)
 
 void	ft_check_error(int argc, char **argv)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (++i < argc)
@@ -64,13 +66,12 @@ void	ft_check_error(int argc, char **argv)
 		{
 			if (ft_strcmp(argv[i], argv[j]))
 			{
-					write(2, "Error\n", 6);
-					exit(0);
+				write(2, "Error\n", 6);
+				exit(0);
 			}
 		}
 	}
 }
-
 
 int		main(int argc, char **argv)
 {
