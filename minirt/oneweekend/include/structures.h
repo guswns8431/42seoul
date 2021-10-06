584cce7d28b06c11abff2de23a09724b990458df
guswns8431 <guswns8431@gmail.com>
2021-10-06 20:51:02 +0900

    object

--- minirt/oneweekend/include/structures.h
+++ minirt/oneweekend/include/structures.h
@@ -7,11 +7,14 @@ typedef struct s_vec3	t_color3;
 typedef struct s_ray	t_ray;
 typedef struct s_camera t_camera;
 typedef struct s_canvas t_canvas;
+typedef struct s_object t_object;
 typedef struct s_sphere t_sphere;
 typedef int				t_bool;
 typedef struct s_hit_record t_hit_record;
 # define FALSE 0
 # define TRUE 1
+typedef int t_object_type;
+# define SP 0
 
 struct s_vec3
 {
@@ -58,6 +61,13 @@ struct s_hit_record{
 	double tmax;
 	double t;
 	t_bool front_face;
+};
+
+struct s_object
+{
+	t_object_type type;
+	void *element;
+	void *next;
 };
 
 #endif
