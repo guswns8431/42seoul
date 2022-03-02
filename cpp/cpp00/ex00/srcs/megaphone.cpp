5014dd36e50092facc1afc6fbd4307ea03e957ec
guswns8431 <guswns8431@gmail.com>
2022-03-02 18:51:12 +0900

    cpp00

--- cpp/cpp00/ex00/srcs/megaphone.cpp
+++ cpp/cpp00/ex00/srcs/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 17:37:26 by hyson             #+#    #+#             */
-/*   Updated: 2022/03/01 22:55:41 by hyson            ###   ########.fr       */
+/*   Updated: 2022/03/02 18:32:54 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@
 int main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "* LOUT AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUT AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
 			{
-				std::cout << (char)std::toupper(argv[i][j]) << std::endl;
+				std::cout << (char)std::toupper(argv[i][j]);
 			}
 		}
 	}
+	std::cout << std::endl;
 	return (0);
 }
