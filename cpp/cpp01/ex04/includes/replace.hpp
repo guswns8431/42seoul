59578ccca5511737ed2cea2660c1a65b3f580154
guswns8431 <guswns8431@gmail.com>
2022-04-02 16:00:59 +0900

    ex04

--- cpp/cpp01/ex04/includes/replace.hpp
+++ cpp/cpp01/ex04/includes/replace.hpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 16:36:28 by hyson             #+#    #+#             */
-/*   Updated: 2022/04/02 15:13:01 by hyson            ###   ########.fr       */
+/*   Updated: 2022/04/02 16:00:47 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 # define RED		"\033[0;31m"
 # define GREEN		"\033[0;32m"
 # define YELLOW		"\033[1;33m"
-# define BLUE		"\033[0;34m"
 # define EOC		"\033[0;0m"
 
 void	ExitInvalidArg(void);
