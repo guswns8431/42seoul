040fbd952b2e4c4619bd671f15cf52a97a86e251
son <guswns8431@gmail.com>
2022-04-22 21:24:22 +0900

    ex00

--- cpp/cpp03/ex00/includes/ClapTrap.hpp
+++ cpp/cpp03/ex00/includes/ClapTrap.hpp
@@ -6,14 +6,28 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 20:18:19 by hyson             #+#    #+#             */
-/*   Updated: 2022/04/21 20:50:15 by hyson            ###   ########.fr       */
+/*   Updated: 2022/04/22 20:40:17 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef "CLAPTRAP_HPP"
-# define "CLAPTRAP_HPP"
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
 
 # include <iostream>
+
+# define RED		"\033[0;31m"
+# define BRED		"\033[0;41m"
+# define GREEN		"\033[0;32m"
+# define BGREEN		"\033[0;42m"
+# define YELLOW		"\033[1;33m"
+# define BLUE		"\033[0;34m"
+# define CYAN		"\033[0;36m"
+# define PURPLE		"\033[0;35m"
+# define BPURPLE	"\033[0;45m"
+# define EOC		"\033[0;0m"
+
+# define HPMAX		100
+# define DMMAX		9999
 
 class ClapTrap {
 	private:
@@ -25,7 +39,7 @@ class ClapTrap {
 		ClapTrap(void);
 		ClapTrap(const ClapTrap& c);
 		ClapTrap(std::string name);
-		~ClapTrap();
+		~ClapTrap(void);
 		ClapTrap&	operator=(const ClapTrap& c);
 		void	attack(const std::string& target);
 		void	takeDamage(unsigned int	amount);
--- cpp/cpp03/ex00/srcs/main.cpp
+++ cpp/cpp03/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 20:19:16 by hyson             #+#    #+#             */
-/*   Updated: 2022/04/21 20:19:44 by hyson            ###   ########.fr       */
+/*   Updated: 2022/04/22 20:56:16 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,58 @@
 
 int main(void)
 {
+	ClapTrap C0;
+	ClapTrap C1("a");
+	ClapTrap C2("b");
+	ClapTrap C3(C1);
+	ClapTrap C4;
+
+	C4 = C2;
+
+	std::cout << std::endl;
+	std::cout << "[ C0 state(default) ]" << std::endl;
+	C0.attack("someone1");
+	C0.takeDamage(3);
+	C0.berepaired(10);
+
+	std::cout << std::endl;
+	std::cout << "[ C1 state(C1 is dead after takeDamage) ]" << std::endl;
+	C1.attack("someone2");
+	C1.takeDamage(11);
+	C1.attack("someone2");
+
+	std::cout << std::endl;
+	std::cout << "[ C2 state(C2 is dead after takeDamage & repaired) ]" << std::endl;
+	C2.takeDamage(11);
+	C2.attack("someone3");
+	C2.berepaired(21);
+	C2.attack("someone4");
+
+	std::cout << std::endl;
+	std::cout << "[ C3 state(C3 has not enough energy & not dead) ]" << std::endl;
+	C3.attack("someone5");
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(1);
+	C3.berepaired(10);
+	C3.berepaired(20);
+	C3.berepaired(30);
+	C3.takeDamage(11);
+	C3.attack("someone6");
+
+	std::cout << std::endl;
+	std::cout << "[ C4 state(C4 can't repaired over 100) ]" << std::endl;
+	C4.berepaired(1000);
+	C4.takeDamage(110);
+	C4.berepaired(500);
+	C4.takeDamage(999999);
+	C4.attack("someone7");
+	std::cout << std::endl;
+
+	return (0);
 }
