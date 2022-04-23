b17fde048fa26a23119291c7febee4a4375e0608
son <guswns8431@gmail.com>
2022-04-23 13:57:16 +0900

    ex01

---
+++ cpp/cpp03/ex01/includes/ScavTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyson <hyson@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 09:27:27 by hyson             #+#    #+#             */
+/*   Updated: 2022/04/23 13:44:17 by hyson            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap(void);
+		ScavTrap(const ScavTrap& s);
+		ScavTrap(const std::string name);
+		~ScavTrap(void);
+		ScavTrap&	operator=(const ScavTrap& s);
+		void		attack(const std::string& target);
+		void		guardGate(void);
+};
+
+#endif
