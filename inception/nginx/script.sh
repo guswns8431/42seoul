40c91a874ea4ee27459c33de13bd7beae3fc50f6
guswns8431 <guswns8431@gmail.com>
2022-06-24 20:04:59 +0900

    nginx foreground + rm tty option

--- inception/nginx/script.sh
+++ inception/nginx/script.sh
@@ -2,7 +2,5 @@
 
 mv ./tmp/default /etc/nginx/sites-available/default
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/server_pkey.pem -out /etc/ssl/certs/server.crt -subj "/C=KR/ST=Seoul/L=YangCheon-gu/O=42/OU=42Seoul/CN=hyson.42.fr"
-service nginx start
-service nginx status
 
-bash #지워야함
+nginx -g 'daemon off;'
