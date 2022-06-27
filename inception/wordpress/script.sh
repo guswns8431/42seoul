b3cb3b51f2211384de72e16ea1fc0eedbeeeafb6
guswns8431 <guswns8431@gmail.com>
2022-06-27 20:08:21 +0900

    wordpress 설정 완료

--- inception/wordpress/script.sh
+++ inception/wordpress/script.sh
@@ -1,11 +1,17 @@
 #!bin/bash
 
-wget https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz
-mv /wordpress/* /var/www/html/
-chown -R www-data:www-data /var/www/html
-rm -r /wordpress latest.tar.gz
-mv ./tmp/wp-config.php /var/www/html/wp-config.php
+#wget https://wordpress.org/latest.tar.gz
+#tar -xvf latest.tar.gz
+#mv /wordpress/* /var/www/html/
+#chown -R www-data:www-data /var/www/html
+#rm -r /wordpress latest.tar.gz
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --allow-root --path=/var/www/html
+mv ./tmp/wp-config.php /var/www/html/
+wp core install --allow-root --url=${MARIADB_USER}.42.fr --title=${WORDPRESS_TITLE} --admin_user=${MARIADB_USER} --admin_password=${MARIADB_PASS} --admin_email=${WORDPRESS_EMAIL} --path=/var/www/html
+wp user create --allow-root ${WORDPRESS_USER_LOGIN} ${WORDPRESS_USER_EMAIL} --user_pass=${WORDPRESS_USER_PASS} --path=/var/www/html
 mv ./tmp/www.conf /etc/php/7.3/fpm/pool.d
 
 echo "env[MARIADB_NAME] = $MARIADB_NAME" >> /etc/php/7.3/fpm/pool.d/www.conf
