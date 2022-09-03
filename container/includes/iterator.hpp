4c43229a22e8f16dddc3ad9c603fa33ef7d7c874
guswns8431 <guswns8431@gmail.com>
2022-09-03 18:16:14 +0900

    🎨 iterator.hpp - fix code

--- container/includes/iterator.hpp
+++ container/includes/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 18:06:45 by hyson             #+#    #+#             */
-/*   Updated: 2022/09/03 18:13:06 by hyson            ###   ########.fr       */
+/*   Updated: 2022/09/03 18:15:27 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 template<typename Category,typename T, typename Distance = std::ptrdiff_t, typename Pointer = T*, typename Reference = T&> //TODO Distance 잘 모르겠음
 struct iterator
 {
-	typedef T        value_type;
-    typedef Distance  difference_type;
-    typedef Pointer   pointer;
-    typedef Reference reference;
-    typedef Category  iterator_category;
+	typedef T			value_type;
+    typedef Distance	difference_type;
+    typedef Pointer		pointer;
+    typedef Reference	reference;
+    typedef Category	iterator_category;
 };
 
 #endif
