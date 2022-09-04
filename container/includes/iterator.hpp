08fba6fe628ce2e030df6d1beefbe3e233a6f3bd
guswns8431 <guswns8431@gmail.com>
2022-09-04 21:39:57 +0900

    ✅ iterator_trait.hpp - iterator.hpp구현으로 std 대신 ft namespace 추가  iterator.hpp - iterator 구현  random_access_iterator.hpp

--- container/includes/iterator.hpp
+++ container/includes/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 18:06:45 by hyson             #+#    #+#             */
-/*   Updated: 2022/09/03 18:15:27 by hyson            ###   ########.fr       */
+/*   Updated: 2022/09/03 21:03:17 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,31 @@
 
 # include <cstddef>
 
-template<typename Category,typename T, typename Distance = std::ptrdiff_t, typename Pointer = T*, typename Reference = T&> //TODO Distance 잘 모르겠음
-struct iterator
+namespace ft
 {
-	typedef T			value_type;
-    typedef Distance	difference_type;
-    typedef Pointer		pointer;
-    typedef Reference	reference;
-    typedef Category	iterator_category;
-};
+/*------------------------------------------------------------------------------*/
+/*								I T E R A T O R									*/
+/*------------------------------------------------------------------------------*/
 
+	template<typename Category,typename T, typename Distance = std::ptrdiff_t, typename Pointer = T*, typename Reference = T&> //TODO Distance 잘 모르겠음
+	struct iterator
+	{
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+		typedef Category	iterator_category;
+	};
+
+/*------------------------------------------------------------------------------*/
+/*							I T E R A T O R _ T A G								*/
+/*------------------------------------------------------------------------------*/
+
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+}
 #endif
--- container/includes/iterator_traits.hpp
+++ container/includes/iterator_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 14:57:43 by hyson             #+#    #+#             */
-/*   Updated: 2022/09/02 15:09:17 by hyson            ###   ########.fr       */
+/*   Updated: 2022/09/03 21:11:57 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define ITERATOR_TRAITS_HPP
 
 #include <cstddef>
-#include <iterator>
+#include "iterator.hpp" //TODO random_access_iterator_tag 따로 구현하지말고 iterator 라이브러리에서 쓰는게 나으려나
 
 /*------------------------------------------------------------------------------*/
 /*						I T E R A T O R _ T R A I T S							*/
@@ -37,7 +37,7 @@ namespace ft {
 		typedef T value_type;
 		typedef T* pointer;
 		typedef T& reference;
-		typedef std::random_access_iterator_tag iterator_category;
+		typedef ft::random_access_iterator_tag iterator_category;
 	};
 
 	template <typename T>
@@ -47,7 +47,7 @@ namespace ft {
 	 	typedef T value_type;
 		typedef const T* pointer;
 		typedef const T& reference;
-		typedef std::random_access_iterator_tag iterator_category;
+		typedef ft::random_access_iterator_tag iterator_category;
 	};
 }
 
