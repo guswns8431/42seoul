eb0a5b6cb4b1640a77202368f8928debacefd823
guswns8431 <guswns8431@gmail.com>
2022-09-08 21:43:54 +0900

    ✅ algorithm.hpp - lexicographical_compare

--- container/includes/algorithm.hpp
+++ container/includes/algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:24:09 by hyson             #+#    #+#             */
-/*   Updated: 2022/09/06 21:41:44 by hyson            ###   ########.fr       */
+/*   Updated: 2022/09/08 21:43:31 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,12 @@ namespace ft
 		{
 			if (!(*first1 == *first2))
 			{
-				return false;
+				return (false);
 			}
 			first1++;
 			first2++;
 		}
-		return true;
+		return (true);
 	}
 
 	template <class InputIt1, class InputIt2, class BinaryPredicate>
@@ -44,34 +44,36 @@ namespace ft
 		{
 			if (!pred(*first1, *first2))
 			{
-				return false;
+				return (false);
 			}
 			first1++;
 			first2++;
 		}
-		return true;
+		return (true);
 	}
 
 /*------------------------------------------------------------------------------*/
 /*					L E X I C O G R A P H I C A L _ C O M P A R E				*/
 /*------------------------------------------------------------------------------*/
+	//COMMENT 두 구간의 대응되는 요소를 차례대로 비교하는데 첫 번째 요소가 두 번째 요소보다 작으면 true를 리턴하고 크면 false를 리턴하며 즉시 종료
+	//두 컨테이너의 요소를 사전식 순으로 비교, 두번째 컨테이너의 요소가 더 크면 참
 	template <class InputIt1, class InputIt2>
-	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2) //두 컨테이너의 요소를 사전식 순으로 비교, 두번째 컨테이너의 요소가 더 크면 참
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
 	{
 		while (first2 != last2) //FIXED for문에서 while문으로 바꿈
 		{
 			if (first1 == last1 || *first1 < *first2)
 			{
-				return true;
+				return (true);
 			}
 			if (*first1 > *first2)
 			{
-				return false;
+				return (false);
 			}
 			first1++;
 			first2++;
 		}
-		return false;
+		return (false);
 	}
 
 	template <class InputIt1, class InputIt2, class Compare>
@@ -81,16 +83,16 @@ namespace ft
 		{
 			if (first1 == last1 || comp(*first1, *first2))
 			{
-				return true;
+				return (true);
 			}
 			if (comp(*first2, *first1))
 			{
-				return false;
+				return (false);
 			}
 			first1++;
 			first2++;
 		}
-		return false;
+		return (false);
 	}
 }
 
