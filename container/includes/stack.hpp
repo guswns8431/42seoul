a4aa7300494d7023d81d92d4b1591adfd93e0e55
guswns8431 <guswns8431@gmail.com>
2022-09-08 23:17:02 +0900

    ✅ stack.hpp - stack 주석 추가 및 테스터 확인

--- container/includes/stack.hpp
+++ container/includes/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: hyson <hyson@42student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 22:38:12 by hyson             #+#    #+#             */
-/*   Updated: 2022/09/08 22:46:49 by hyson            ###   ########.fr       */
+/*   Updated: 2022/09/08 23:15:01 by hyson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,66 +34,103 @@ namespace ft
 /*--------------------------------------------------------------------------*/
 /*				C O N S T R U C T O R _ & _ D E S T R U C T O R				*/
 /*--------------------------------------------------------------------------*/
-	explicit stack(const container_type& container = container_type()) : c(container) {}
-	stack(const stack& s) : c(s.c) {}
-	~stack(void) {}
+			explicit stack(const container_type& container = container_type()) : c(container) {}
+			stack(const stack& s) : c(s.c) {}
+			~stack(void) {}
 
-	/* member function for util */
-	stack& operator=(const stack& s) {
-	if (this != &s) {
-	c = s.c;
-	}
-	return *this;
-	}
+/*--------------------------------------------------------------------------*/
+/*						M E M B E R _ F U N C T I O N						*/
+/*--------------------------------------------------------------------------*/
+			stack& operator=(const stack& s)
+			{
+				if (this != &s)
+				{
+					c = s.c;
+				}
+				return (*this);
+			}
 
-	/* element access */
-	reference top(void) {
-	return c.back();
+/*--------------------------------------------------------------------------*/
+/*						E L E M E N T _ A C C E S S							*/
+/*--------------------------------------------------------------------------*/
+	reference top(void)
+	{
+		return (c.back());
 	}
-	const_reference top(void) const {
-	return c.back();
-	}
-
-	/* capacity */
-	bool empty(void) const {
-	return c.empty();
-	}
-
-	size_type size(void) const {
-	return c.size();
-	}
-
-	/* modifiers */
-	void push(const value_type& value) {
-	c.push_back(value);
-	}
-	void pop(void) {
-	c.pop_back();
+	const_reference top(void) const
+	{
+		return (c.back());
 	}
 
-	/* friend non member function for relational operators */
-	friend bool operator==(const stack& x, const stack& y) {
-	return x.c == y.c;
+/*--------------------------------------------------------------------------*/
+/*								C A P A C I T Y								*/
+/*--------------------------------------------------------------------------*/
+	bool empty(void) const
+	{
+		return (c.empty());
 	}
-	friend bool operator!=(const stack& x, const stack& y) {
-	return x.c != y.c;
+
+	size_type size(void) const
+	{
+		return (c.size());
 	}
-	friend bool operator<(const stack& x, const stack& y) {
-	return x.c < y.c;
+
+/*--------------------------------------------------------------------------*/
+/*							M O D I F I E R	S								*/
+/*--------------------------------------------------------------------------*/
+	void push(const value_type& value)
+	{
+		c.push_back(value);
 	}
-	friend bool operator<=(const stack& x, const stack& y) {
-	return x.c <= y.c;
-	}
-	friend bool operator>(const stack& x, const stack& y) {
-	return x.c > y.c;
-	}
-	friend bool operator>=(const stack& x, const stack& y) {
-	return x.c >= y.c;
+	void pop(void)
+	{
+		c.pop_back();
 	}
 
 	protected:
-	container_type c;
+		container_type c;
+
+	//COMMNET c가 protected라서 그것을 쓸 수 있게 하기 위하여 friend 키워드 사용
+	template < class Tn, class Containern >
+	friend bool operator== (const stack< Tn,Containern >& x, const stack< Tn,Containern >& y);
+	template < class Tn, class Containern >
+	friend bool operator<  (const stack< Tn,Containern >& x, const stack< Tn,Containern >& y);
+
 	};
+
+/*--------------------------------------------------------------------------*/
+/*					N O N _ M E M B E R _ F U N C T I O N S					*/
+/*--------------------------------------------------------------------------*/
+	template <class Tn, class Containern>
+	bool operator== (const stack<Tn,Containern>& x, const stack<Tn,Containern>& y)
+	{
+		return (x.c == y.c);
+	}
+	template <class Tn, class Containern>
+	bool operator<  (const stack<Tn,Containern>& x, const stack<Tn,Containern>& y)
+	{
+		return (x.c < y.c);
+	}
+	template <class Tn, class Containern>
+	bool operator!= (const stack<Tn,Containern>& x, const stack<Tn,Containern>& y)
+	{
+		return !(x == y);
+	}
+	template <class Tn, class Containern>
+	bool operator<= (const stack<Tn,Containern>& x, const stack<Tn,Containern>& y)
+	{
+		return !(y < x);
+	}
+	template <class Tn, class Containern>
+	bool operator>  (const stack<Tn,Containern>& x, const stack<Tn,Containern>& y)
+	{
+		return (y < x);
+	}
+	template <class Tn, class Containern>
+	bool operator>= (const stack<Tn,Containern>& x, const stack<Tn,Containern>& y)
+	{
+		return !(x < y);
+	}
 
 }
 
